8c2f44a86e45948dc0ac01d125f4f7366dd78402
Harendra Kumar <harendra@composewell.com>
2020-04-30 13:06:44 +0530

    bump script version

--- packcheck.sh
+++ packcheck.sh
@@ -20,7 +20,7 @@
 # Utility functions
 #------------------------------------------------------------------------------
 
-PACKCHECK_VERSION=0.4.3
+PACKCHECK_VERSION=0.5.0
 
 show_version() {
   echo "packcheck version $PACKCHECK_VERSION"
